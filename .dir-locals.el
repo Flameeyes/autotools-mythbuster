ea131c3ba96e6eb46ac846489db1b4f3c23685a4
Diego Elio Pettenò <flameeyes@flameeyes.eu>
2014-09-13 21:41:03 +0100

    Add a .dir-locals.el file for Emacs.

---
+++ .dir-locals.el
@@ -0,0 +1,6 @@
+((nil . ((indent-tabs-mode . nil)))
+ (nxml-mode . ((fill-column . 100)
+	       (ispell-local-dictionary . "british")
+               (mode . auto-fill)
+               (mode . flyspell)))
+ (xml-mode . ((mode . nxml))))
