a1e2f2b01ea8a767a92e1ee58b33401e3bea0cb8
Diego Elio Pettenò <flameeyes@flameeyes.com>
2020-04-19 15:07:23 +0100

    Actually use MIT rather than CC0, for OSI compatibility.

--- examples/autoconf/caching/whynot/thisonlyworksonc99.h
+++ examples/autoconf/caching/whynot/thisonlyworksonc99.h
@@ -1,6 +1,6 @@
 // SPDX-FileCopyrightText: 2009- Diego Elio Pettenò
 //
-// SPDX-License-Identifier: CC0-1.0
+// SPDX-License-Identifier: MIT
 
 #if __STDC_VERSION__ != 199901L
 # error "Sorry this won't work"
